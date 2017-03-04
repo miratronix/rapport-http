5cdb3a0366c2d7e0101e297ecb849bb9018c54e9
Lukas Rezek <lukas@miratronix.com>
2017-03-04 16:57:33 -0500

    Add support for routable events (#4)
    * Add a expectResponse method to allow for firing routable events
    
    * Add unit tests for expectResponse
    
    * Bump version to 0.0.3

--- test/http.test.js
+++ test/http.test.js
@@ -14,191 +14,210 @@ describe('HTTP', () => {
         testSocket = Rapport().use(plugin).wrap(mockSocket);
     });
 
-    context('http', () => {
+    context('body', () => {
 
-        context('body', () => {
-
-            it('Can add a body object', () => {
-                const http = testSocket.http('get', '/test').body({ key: 'value' });
-                http._body.should.have.a.property('key').that.equals('value');
-            });
-
-            it('Can add multiple body objects', () => {
-                const http = testSocket.http('get', '/test')
-                    .body({ key: 'value' })
-                    .body({ key2: 'value2' });
-
-                http._body.should.have.a.property('key').that.equals('value');
-                http._body.should.have.a.property('key2').that.equals('value2');
-            });
-
-            it('Can add a non-object body', () => {
-                const http = testSocket.http('get', '/test').body('hello');
-                http._body.should.equal('hello');
-            });
-
-            it('Overwrites the body with a non-object body', () => {
-                const http = testSocket.http('get', '/test')
-                    .body({ key: 'value' })
-                    .body('some string');
-
-                http._body.should.equal('some string');
-            });
+        it('Can add a body object', () => {
+            const http = testSocket.http('get', '/test').body({ key: 'value' });
+            http._body.should.have.a.property('key').that.equals('value');
         });
 
-        context('query', () => {
+        it('Can add multiple body objects', () => {
+            const http = testSocket.http('get', '/test')
+                .body({ key: 'value' })
+                .body({ key2: 'value2' });
 
-            it('Can add a query object', () => {
-                const http = testSocket.http('get', '/test').query({ key: 'value' });
-                http._url.should.equal('/test?key=value');
-            });
-
-            it('Can add multiple query objects', () => {
-                const http = testSocket.http('get', '/test')
-                    .query({ key: 'value' })
-                    .query({ keys: 'values' });
-
-                http._url.should.equal('/test?key=value&keys=values');
-            });
-
-            it('Can add a string query', () => {
-                const http = testSocket.http('get', '/test').query('key=value');
-                http._url.should.equal('/test?key=value');
-            });
-
-            it('Can add an array query', () => {
-                const http = testSocket.http('get', '/test').query(['key=value', 'keys=values']);
-                http._url.should.equal('/test?key=value&keys=values');
-            });
-
-            it('Appends the previous query with a query string', () => {
-                const http = testSocket.http('get', '/test')
-                    .query({ key: 'value' })
-                    .query('keys=values');
-
-                http._url.should.equal('/test?key=value&keys=values');
-            });
-
-            it('Appends the previous query with a query array', () => {
-                const http = testSocket.http('get', '/test')
-                    .query({ key: 'value' })
-                    .query(['keys=values']);
-
-                http._url.should.equal('/test?key=value&keys=values');
-            });
-
-            it('Throws if the query isn\'t an object, array, or string', () => {
-                (() => {
-                    testSocket.http('get', '/test').query(42);
-                }).should.throw(TypeError, 'Query must be a url encoded string, object, or array');
-            });
+            http._body.should.have.a.property('key').that.equals('value');
+            http._body.should.have.a.property('key2').that.equals('value2');
         });
 
-        context('timeout', () => {
-
-            it('Can set a timeout', () => {
-                const http = testSocket.http('get', '/test').timeout(42);
-                http._timeout.should.equal(42);
-            });
-
-            it('Ignores a negative timeout', () => {
-                const http = testSocket.http('get', '/test').timeout(-42);
-                http._timeout.should.equal(0);
-            });
-
-            it('Throws when anything but a number is supplied', () => {
-                (() => {
-                    testSocket.http('get', '/test').timeout('lol');
-                }).should.throw(TypeError, 'Timeout must be a numeric value');
-            });
+        it('Can add a non-object body', () => {
+            const http = testSocket.http('get', '/test').body('hello');
+            http._body.should.equal('hello');
         });
 
-        context('send', () => {
+        it('Overwrites the body with a non-object body', () => {
+            const http = testSocket.http('get', '/test')
+                .body({ key: 'value' })
+                .body('some string');
 
-            it('Sends a request with a promise', () => {
-                return new Promise((resolve) => {
-                    testSocket.http('get', '/test').send().then(resolve);
-                    mockSocket.fire('message', {
-                        responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
-                        body: {}
-                    });
-                });
-            });
+            http._body.should.equal('some string');
+        });
+    });
 
-            it('Sends a request with a callback', () => {
-                return new Promise((resolve) => {
-                    testSocket.http('get', '/test').send(resolve);
-                    mockSocket.fire('message', {
-                        responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
-                        body: {}
-                    });
-                });
-            });
+    context('query', () => {
 
-            it('Sends the request method', () => {
-                return new Promise((resolve) => {
-                    testSocket.http('get', '/test').send().then(resolve);
-                    mockSocket.fire('message', {
-                        responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
-                        body: {}
-                    });
-                }).then(() => {
-                    JSON.parse(mockSocket.lastSentMessage).body.should.have.a.property('method').that.equals('get');
-                });
-            });
-
-            it('Sends the request URL', () => {
-                return new Promise((resolve) => {
-                    testSocket.http('get', '/test').send().then(resolve);
-                    mockSocket.fire('message', {
-                        responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
-                        body: {}
-                    });
-                }).then(() => {
-                    JSON.parse(mockSocket.lastSentMessage).body.should.have.a.property('url').that.equals('/test');
-                });
-            });
-
-            it('Sends the request body', () => {
-                return new Promise((resolve) => {
-                    testSocket.http('get', '/test').body('hello').send().then(resolve);
-                    mockSocket.fire('message', {
-                        responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
-                        body: {}
-                    });
-                }).then(() => {
-                    JSON.parse(mockSocket.lastSentMessage).body.should.have.a.property('body').that.equals('hello');
-                });
-            });
-
-            it('Sends the request query', () => {
-                return new Promise((resolve) => {
-                    testSocket.http('get', '/test').query('key=value').send().then(resolve);
-                    mockSocket.fire('message', {
-                        responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
-                        body: {}
-                    });
-                }).then(() => {
-                    JSON.parse(mockSocket.lastSentMessage).body.should.have.a.property('url').that.equals('/test?key=value');
-                });
-            });
-
-            it('Uses the request timeout', () => {
-                return testSocket.http('get', '/test').timeout(10).send()
-                    .should.be.rejectedWith(Error, 'Timed out after 10 ms');
-            });
+        it('Can add a query object', () => {
+            const http = testSocket.http('get', '/test').query({ key: 'value' });
+            http._url.should.equal('/test?key=value');
         });
 
-        it('Throws if the method is not a string', () => {
+        it('Can add multiple query objects', () => {
+            const http = testSocket.http('get', '/test')
+                .query({ key: 'value' })
+                .query({ keys: 'values' });
+
+            http._url.should.equal('/test?key=value&keys=values');
+        });
+
+        it('Can add a string query', () => {
+            const http = testSocket.http('get', '/test').query('key=value');
+            http._url.should.equal('/test?key=value');
+        });
+
+        it('Can add an array query', () => {
+            const http = testSocket.http('get', '/test').query(['key=value', 'keys=values']);
+            http._url.should.equal('/test?key=value&keys=values');
+        });
+
+        it('Appends the previous query with a query string', () => {
+            const http = testSocket.http('get', '/test')
+                .query({ key: 'value' })
+                .query('keys=values');
+
+            http._url.should.equal('/test?key=value&keys=values');
+        });
+
+        it('Appends the previous query with a query array', () => {
+            const http = testSocket.http('get', '/test')
+                .query({ key: 'value' })
+                .query(['keys=values']);
+
+            http._url.should.equal('/test?key=value&keys=values');
+        });
+
+        it('Throws if the query isn\'t an object, array, or string', () => {
             (() => {
-                testSocket.http({}, '/test');
-            }).should.throw(TypeError, 'HTTP method must be a string');
+                testSocket.http('get', '/test').query(42);
+            }).should.throw(TypeError, 'Query must be a url encoded string, object, or array');
+        });
+    });
+
+    context('timeout', () => {
+
+        it('Can set a timeout', () => {
+            const http = testSocket.http('get', '/test').timeout(42);
+            http._timeout.should.equal(42);
         });
 
-        it('Throws if the url is not a string', () => {
-            (() => {
-                testSocket.http('hello', {});
-            }).should.throw(TypeError, 'HTTP URL must be a string');
+        it('Ignores a negative timeout', () => {
+            const http = testSocket.http('get', '/test').timeout(-42);
+            http._timeout.should.equal(0);
         });
+
+        it('Throws when anything but a number is supplied', () => {
+            (() => {
+                testSocket.http('get', '/test').timeout('lol');
+            }).should.throw(TypeError, 'Timeout must be a numeric value');
+        });
+    });
+
+    context('expectResponse', () => {
+
+        it('Sets the expect response flag', () => {
+            const http = testSocket.http('get', '/test').expectResponse(false);
+            http._expectResponse.should.equal(false);
+        });
+    });
+
+    context('send', () => {
+
+        it('Sends a request with a promise', () => {
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').send().then(resolve);
+                mockSocket.fire('message', {
+                    responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
+                    body: {}
+                });
+            });
+        });
+
+        it('Sends a request with a callback', () => {
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').send(resolve);
+                mockSocket.fire('message', {
+                    responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
+                    body: {}
+                });
+            });
+        });
+
+        it('Sends the request method', () => {
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').send().then(resolve);
+                mockSocket.fire('message', {
+                    responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
+                    body: {}
+                });
+            }).then(() => {
+                JSON.parse(mockSocket.lastSentMessage).body.should.have.a.property('method').that.equals('get');
+            });
+        });
+
+        it('Sends the request URL', () => {
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').send().then(resolve);
+                mockSocket.fire('message', {
+                    responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
+                    body: {}
+                });
+            }).then(() => {
+                JSON.parse(mockSocket.lastSentMessage).body.should.have.a.property('url').that.equals('/test');
+            });
+        });
+
+        it('Sends the request body', () => {
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').body('hello').send().then(resolve);
+                mockSocket.fire('message', {
+                    responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
+                    body: {}
+                });
+            }).then(() => {
+                JSON.parse(mockSocket.lastSentMessage).body.should.have.a.property('body').that.equals('hello');
+            });
+        });
+
+        it('Sends the request query', () => {
+            return new Promise((resolve) => {
+                testSocket.http('get', '/test').query('key=value').send().then(resolve);
+                mockSocket.fire('message', {
+                    responseId: JSON.parse(mockSocket.lastSentMessage).requestId,
+                    body: {}
+                });
+            }).then(() => {
+                JSON.parse(mockSocket.lastSentMessage).body.should.have.a.property('url').that.equals('/test?key=value');
+            });
+        });
+
+        it('Uses the request timeout', () => {
+            return testSocket.http('get', '/test').timeout(10).send()
+                .should.be.rejectedWith(Error, 'Timed out after 10 ms');
+        });
+
+        it('Sends the request with a request ID of "event" when no response is expected', () => {
+            testSocket.http('get', '/test').expectResponse(false).send();
+            JSON.parse(mockSocket.lastSentMessage).should.have.a.property('requestId').that.equals('event');
+        });
+
+        it('Sends the request object as the body when no response is expected', () => {
+            testSocket.http('get', '/test').body('hello').expectResponse(false).send();
+            const lastSentMessage = JSON.parse(mockSocket.lastSentMessage);
+            lastSentMessage.should.have.a.property('body');
+            lastSentMessage.body.should.have.a.property('url').that.equals('/test');
+            lastSentMessage.body.should.have.a.property('method').that.equals('get');
+            lastSentMessage.body.should.have.a.property('body').that.equals('hello');
+        });
+    });
+
+    it('Throws if the method is not a string', () => {
+        (() => {
+            testSocket.http({}, '/test');
+        }).should.throw(TypeError, 'HTTP method must be a string');
+    });
+
+    it('Throws if the url is not a string', () => {
+        (() => {
+            testSocket.http('hello', {});
+        }).should.throw(TypeError, 'HTTP URL must be a string');
     });
 });
